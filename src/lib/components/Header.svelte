b4f85d092a41599507fd2cc933d6ec01c4b41a7b
ReinaldoGritspa <juniorgritspa@gmail.com>
2025-06-18 10:56:56 -0400

    ex1

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,9 @@
-<header class="navbar navbar-expand-lg bg-body-tertiary">
-  <button class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#mySideBar" aria-label="toggleSidebar">
+<header class="navbar navbar-expand-lg">
+  <button class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#mySideBar" aria-label="Toggle Sidebar">
     <i class="bi bi-list"></i>
   </button>
-  <a class="navbar-brand" href="/"> Página Inicial </a>
-  <button class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#navbarText" aria-label="toggleNavbar">
+  <a class="navbar-brand" href="/">Página Inicial</a>
+  <button class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#navbarText" aria-label="Toggle Navbar">
     <i class="bi bi-three-dots-vertical"></i>
   </button>
   <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarText">
@@ -14,13 +14,24 @@
     <div class="offcanvas-body">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active"  href="/03/external/dummy/users"> Users </a>
+          <a class="nav-link active" href="/03/external/dummy/users">
+            <i class="bi bi-person-circle me-2"></i> Usuários
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="/03/pokemon"> Pokemon </a>
+          <a class="nav-link active" href="/03/pokemon">
+            <i class="bi bi-joystick me-2"></i> Pokémon
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/03/formulario"> Formulario </a>
+          <a class="nav-link" href="/03/formulario">
+            <i class="bi bi-file-earmark-post me-2"></i> Formulário
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/06/post">
+            <i class="bi bi-envelope-at me-2"></i> Senha e E-mail
+          </a>
         </li>
       </ul>
     </div>
